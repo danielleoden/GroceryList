c03d663c623460e8fcedfedd794f5342f2b4a392
DESKTOP-N460M9B\Danielle Oden <doden@mail.csuchico.edu>
2016-11-07 16:40:26 -0800

    missplaced a semi colon

--- groceries.js
+++ groceries.js
@@ -8,7 +8,7 @@ function addItem()
   list.appendChild(item);
   document.getElementById("newItem").value = "";
 }
-//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+//courtesy of w3schoolss, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
